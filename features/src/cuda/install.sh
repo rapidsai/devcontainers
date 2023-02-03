8512c5de80e448a0296ae5302dbfa937d971d9d6
ptaylor <paul.e.taylor@me.com>
2023-02-03 13:53:40 -0800

    install libcutensor from focal repo

--- features/src/cuda/install.sh
+++ features/src/cuda/install.sh
@@ -24,12 +24,20 @@ if [[ "$NVARCH" == aarch64 ]]; then
 fi
 
 # Add NVIDIA's keyring and apt repository
-wget --no-hsts -q -O /tmp/cuda-keyring_1.0-1_all.deb "\
-https://developer.download.nvidia.com/compute/cuda/repos/\
-$(. /etc/os-release; echo "$ID${VERSION_ID/./}")/\
-${NVARCH}/cuda-keyring_1.0-1_all.deb";
+cuda_repo_base="https://developer.download.nvidia.com/compute/cuda/repos";
+cuda_repo="${cuda_repo_base}/$(. /etc/os-release; echo "$ID${VERSION_ID/./}")/${NVARCH}";
 
-dpkg -i /tmp/cuda-keyring_1.0-1_all.deb;
+cuda_keyring_deb="$(
+    curl -s -X GET ${cuda_repo}/Packages \
+  | grep -P '^Filename: \./cuda-keyring(.*)\.deb$' \
+  | sort -Vr | head -n1 | cut -d' ' -f2 \
+)";
+
+cuda_keyring_deb="${cuda_keyring_deb#./}";
+
+wget --no-hsts -q -O /tmp/${cuda_keyring_deb} "${cuda_repo}/${cuda_keyring_deb}";
+
+dpkg -i /tmp/${cuda_keyring_deb};
 
 apt-get update;
 
@@ -43,8 +51,6 @@ cuda_ver="${cuda_ver/./-}";
 
 check_packages                          \
     libnccl-dev                         \
-    `# cutensor isn't 22.04 repo yet?`  \
-    `# libcutensor-dev`                 \
     cuda-compiler-${cuda_ver}           \
     cuda-nvml-dev-${cuda_ver}           \
     cuda-libraries-dev-${cuda_ver}      \
@@ -52,6 +58,35 @@ check_packages                          \
     $([ "$NVARCH" == x86_64 ] && echo   \
         gds-tools-${cuda_ver} || echo ) \
     ;
+
+# HACK: libcutensor-dev isn't currently in the ubuntu22.04 repo,
+# but is in ubuntu20.04. Detect this and download the 20.04 deb.
+if ! dpkg -s libcutensor-dev > /dev/null 2>&1; then
+    # If `libcutensor-deb` is available in the apt repo, install it
+    if ! dpkg -p libcutensor-dev 2>&1 | grep -q "not available" >/dev/null 2>&1; then
+        check_packages libcutensor-dev;
+    else
+        # If it's not in the apt repo for the current OS version, install it from the 20.04 repo
+        focal_cuda_repo="${cuda_repo_base}/ubuntu2004/${NVARCH}";
+        wget --no-hsts -q -O /tmp/focal-cuda-packages "${focal_cuda_repo}/Packages";
+
+        get_focal_cuda_deb() {
+            local deb="$(                          \
+                cat /tmp/focal-cuda-packages       \
+            | grep -P "^Filename: \./$1(.*)\.deb$" \
+            | sort -Vr | head -n1 | cut -d' ' -f2  \
+            )";
+            wget --no-hsts -q -O "/tmp/${deb#./}" "${focal_cuda_repo}/${deb#./}";
+            echo -n "/tmp/${deb#./}";
+        }
+
+        DEBIAN_FRONTEND=noninteractive \
+        apt install -y --no-install-recommends      \
+            "$(get_focal_cuda_deb libcutensor1)"    \
+            "$(get_focal_cuda_deb libcutensor-dev)" \
+            ;
+    fi
+fi
 
 if [[ ! -L "${CUDA_HOME}" ]]; then
     # Create /usr/local/cuda symlink
