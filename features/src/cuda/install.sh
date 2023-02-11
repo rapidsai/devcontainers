7fb48c8036d09115ea9dd7f1931433d0db7dbebe
ptaylor <paul.e.taylor@me.com>
2023-02-10 18:43:27 -0800

    add option to prune CTK static libs for smaller images

--- features/src/cuda/install.sh
+++ features/src/cuda/install.sh
@@ -92,11 +92,33 @@ export CUDA_VERSION="$CUDA_VERSION_MAJOR.$CUDA_VERSION_MINOR.$CUDA_VERSION_PATCH
 
 # Remove extra libcutensor versions
 libcutensor_ver="$(dpkg -s libcutensor1 | grep '^Version:' | cut -d' ' -f2 | cut -d'-' -f1 | cut -d'.' -f4 --complement)";
+libcutensorMg_shared="$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensorMg.so.${libcutensor_ver}$")"
+libcutensorMg_static="$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensorMg_static.a$")"
+libcutensor_shared="$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensor.so.${libcutensor_ver}$")"
+libcutensor_static="$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensor_static.a$")"
 
-update-alternatives --set libcutensorMg.so.${libcutensor_ver} "$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensorMg.so.${libcutensor_ver}$")";
-update-alternatives --set libcutensorMg_static.a              "$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensorMg_static.a$")";
-update-alternatives --set libcutensor.so.${libcutensor_ver}   "$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensor.so.${libcutensor_ver}$")";
-update-alternatives --set libcutensor_static.a                "$(find /usr/lib -type f -regex "^.*/libcutensor/${CUDA_VERSION_MAJOR}/libcutensor_static.a$")";
+libcutensorMg_shared_link="$(update-alternatives --query libcutensorMg.so.${libcutensor_ver} | grep '^Link:' | cut -d' ' -f2)";
+libcutensorMg_static_link="$(update-alternatives --query libcutensorMg_static.a              | grep '^Link:' | cut -d' ' -f2)";
+libcutensor_shared_link="$(update-alternatives --query libcutensor.so.${libcutensor_ver}     | grep '^Link:' | cut -d' ' -f2)";
+libcutensor_static_link="$(update-alternatives --query libcutensor_static.a                  | grep '^Link:' | cut -d' ' -f2)";
+
+# Remove existing libcutensor lib alternatives
+(update-alternatives --remove-all libcutensorMg.so.${libcutensor_ver} >/dev/null 2>&1 || true);
+(update-alternatives --remove-all libcutensorMg_static.a              >/dev/null 2>&1 || true);
+(update-alternatives --remove-all libcutensor.so.${libcutensor_ver}   >/dev/null 2>&1 || true);
+(update-alternatives --remove-all libcutensor_static.a                >/dev/null 2>&1 || true);
+
+# Install only the alternative for the version we keep
+update-alternatives --install "${libcutensorMg_shared_link}" libcutensorMg.so.${libcutensor_ver} "${libcutensorMg_shared}" 0
+update-alternatives --install "${libcutensorMg_static_link}" libcutensorMg_static.a              "${libcutensorMg_static}" 0
+update-alternatives --install "${libcutensor_shared_link}"   libcutensor.so.${libcutensor_ver}   "${libcutensor_shared}"   0
+update-alternatives --install "${libcutensor_static_link}"   libcutensor_static.a                "${libcutensor_static}"   0
+
+# Set the default alternative
+update-alternatives --set libcutensorMg.so.${libcutensor_ver} "${libcutensorMg_shared}";
+update-alternatives --set libcutensorMg_static.a              "${libcutensorMg_static}";
+update-alternatives --set libcutensor.so.${libcutensor_ver}   "${libcutensor_shared}";
+update-alternatives --set libcutensor_static.a                "${libcutensor_static}";
 
 rm -rf $(find /usr/lib -mindepth 1 -type d -regex "^.*/libcutensor/.*$" | grep -Ev "^.*/libcutensor/${CUDA_VERSION_MAJOR}$");
 
@@ -115,3 +137,18 @@ rm -rf /var/cache/apt/*;
 rm -rf /var/lib/apt/lists/*;
 
 rm -rf /tmp/*.deb;
+
+if [[ "${PRUNESTATICLIBS:-false}" == true ]]; then
+
+    rm -rf "${libcutensorMg_static}" "${libcutensor_static}";
+    find /usr/lib -type f -name 'libnccl_static.a' -delete || true;
+
+    (update-alternatives --remove-all libcutensorMg_static.a >/dev/null 2>&1 || true);
+    (update-alternatives --remove-all libcutensor_static.a   >/dev/null 2>&1 || true);
+
+    for dir in "lib" "lib64"; do
+        find "$(realpath -m "${CUDA_HOME}/${dir}")/" -type f \
+            \( -name '*.a' ! -name 'libcudart_static.a' ! -name 'libcudadevrt.a' ! -name 'libculibos.a' \) \
+            -delete || true;
+    done
+fi
