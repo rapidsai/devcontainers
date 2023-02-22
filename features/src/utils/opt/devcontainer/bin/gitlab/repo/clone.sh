13d5af9c8463f10a250a5793d3e4639b1324d2aa
ptaylor <paul.e.taylor@me.com>
2023-02-21 21:21:03 -0800

    update clone scripts to accept extra git args

--- features/src/utils/opt/devcontainer/bin/gitlab/repo/clone.sh
+++ features/src/utils/opt/devcontainer/bin/gitlab/repo/clone.sh
@@ -2,39 +2,49 @@
 
 set -euo pipefail;
 
-. /opt/devcontainer/bin/gitlab/cli/init.sh;
-if [[ -z "$GITLAB_USER" ]]; then exit 1; fi
+clone_gitlab_repo() {
 
-NAME="$2";
-UPSTREAM="$1/$NAME";
-REPO="$GITLAB_USER/$NAME";
-FORK="$(glab repo view "$REPO" &>/dev/null; if [ $? -eq 0 ]; then echo "$REPO"; fi;)";
+    . /opt/devcontainer/bin/gitlab/cli/init.sh;
+    if [[ -z "$GITLAB_USER" ]]; then exit 1; fi
 
-if [[ -z "$FORK" ]]; then
-    ORIGIN_URL="gitlab.com/$REPO";
-    UPSTREAM_URL="gitlab.com/$UPSTREAM";
+    local user="$GITLAB_USER";
+    local orig="${1:?orig is required}"; shift;
+    local repo="${1:?repo is required}"; shift;
+    local dest="${1:-$repo}";
+    local args="${@:1}";
 
-    while true; do
+    local src="${orig}/${repo}";
+    local dst="${user}/${repo}";
 
-        read -p "\`$UPSTREAM_URL\` not found.
-Fork \`$UPSTREAM_URL\` into \`$ORIGIN_URL\` now (y/n)? " CHOICE </dev/tty
+    local fork="$(glab repo view "${dst}" &>/dev/null; \
+        if [ $? -eq 0 ]; then echo "${dst}"; fi;)";
 
-        case $CHOICE in
-            [Nn]* ) REPO="$UPSTREAM"; break;;
-            [Yy]* ) gh repo fork "$UPSTREAM" --clone=false; break;;
-            * ) echo "Please answer 'y' or 'n'";;
-        esac
-        unset CHOICE;
-    done;
-fi
+    if [[ -z "$fork" ]]; then
 
-if [ ! -d ~/${3:-$NAME}/.git ]; then
-    glab repo clone "$REPO" ~/${3:-$NAME};
-fi
+        while true; do
+            read -p "'gitlab.com/${dst}.git' not found.
+Fork '${src}' into '${dst}' now (y/n)? " CHOICE </dev/tty
+            case $CHOICE in
+                [Nn]* ) fork="${src}"; break;;
+                [Yy]* ) fork="${dst}";
+                        gh repo fork "${src}" --clone=false;
+                        break;;
+                * ) echo "Please answer 'y' or 'n'";;
+            esac
+            unset CHOICE;
+        done;
+    fi
 
-unset NAME;
-unset UPSTREAM;
-unset REPO;
-unset FORK;
-unset ORIGIN_URL;
-unset UPSTREAM_URL;
+    if [ ! -d ~/${dest}/.git ]; then
+        glab repo clone ${fork} ~/${dest} ${args:+-- $args};
+    fi
+
+    unset NAME;
+    unset UPSTREAM;
+    unset REPO;
+    unset FORK;
+    unset ORIGIN_URL;
+    unset UPSTREAM_URL;
+}
+
+clone_gitlab_repo "$@";
