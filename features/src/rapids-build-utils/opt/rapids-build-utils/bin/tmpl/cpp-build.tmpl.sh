712bdc0cff329a667e0f84cfc09aa1a6c3dcf6ac
ptaylor <paul.e.taylor@me.com>
2023-02-23 14:09:07 -0800

    regenerate the ldcache after build

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/cpp-build.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/cpp-build.tmpl.sh
@@ -7,6 +7,8 @@ build_${CPP_LIB}_cpp() {
     cmake --build ~/${CPP_SRC}/build/latest      \
         -j${PARALLEL_LEVEL:-$(nproc --ignore=2)} \
     ;
+
+    sudo ldconfig;
 }
 
 build_${CPP_LIB}_cpp "$@";
