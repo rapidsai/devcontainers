3ac5e562b7fa56c3a4be7dda091bb6c6dde6b72b
ptaylor <paul.e.taylor@me.com>
2023-02-27 13:40:56 -0800

    add impl for rapids-make-pip-env

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/update-content-command.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/update-content-command.sh
@@ -3,3 +3,16 @@
 mkdir -m 0755 -p ~/{.aws,.cache,.conda,.config};
 
 rapids-generate-scripts;
+
+if [[ "${PYTHON_PACKAGE_MANAGER:-}" == "pip" ]]; then
+    _activate_venv=$(cat<<"EOF"
+if [[ -f ~/.local/share/venvs/${DEFAULT_VIRTUAL_ENV:-rapids}/bin/activate ]]; then
+    . ~/.local/share/venvs/${DEFAULT_VIRTUAL_ENV:-rapids}/bin/activate;
+fi
+EOF
+);
+    if [[ "$(cat ~/.bashrc)" != *"$_activate_venv"* ]]; then
+        echo "$_activate_venv" >> ~/.bashrc;
+    fi
+    unset _activate_venv;
+fi
