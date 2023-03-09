8e6ede9529820655ac070ed20a4fdeb3372f76eb
ptaylor <paul.e.taylor@me.com>
2023-03-09 21:26:01 +0000

    install the cuda-toolkit package

--- features/src/cuda/install.sh
+++ features/src/cuda/install.sh
@@ -54,12 +54,9 @@ cuda_ver="${cuda_ver/./-}";
 
 check_packages                          \
     libnccl-dev                         \
-    cuda-compiler-${cuda_ver}           \
-    cuda-nvml-dev-${cuda_ver}           \
-    cuda-libraries-dev-${cuda_ver}      \
-    cuda-command-line-tools-${cuda_ver} \
-    $([ "$NVARCH" == x86_64 ] && echo   \
-        gds-tools-${cuda_ver} || echo ) \
+    cuda-toolkit-${cuda_ver}            \
+    $([ "$NVARCH" == x86_64 ]           \
+     && echo nvidia-fs || echo)         \
     ;
 
 # HACK: libcutensor-dev isn't currently in the ubuntu22.04 repo,
---
+++ features/test/cuda/cuda_11-ubuntu18.04.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+# This test can be run with the following command (from the root of this repo)
+# ```
+# npx --package=@devcontainers/cli -c 'devcontainer features test \
+#     --features cuda \
+#     --base-image ubuntu:22.04 .'
+# ```
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+>&2 echo "PATH=$PATH"
+>&2 echo "BASH_ENV=$BASH_ENV"
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "CUDA version" bash -c "echo '$CUDA_VERSION' | grep '11.0.0'";
+check "CUDA major version" bash -c "echo '$CUDA_VERSION_MAJOR' | grep '11'";
+check "CUDA minor version" bash -c "echo '$CUDA_VERSION_MINOR' | grep '0'";
+check "CUDA patch version" bash -c "echo '$CUDA_VERSION_PATCH' | grep '0'";
+check "installed" stat /usr/local/cuda-11.0 /usr/local/cuda
+check "nvcc exists and is on path" which nvcc
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
