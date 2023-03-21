8dcb89a9d502d7800a047b36c823c3e535f43cb1
Paul Taylor <paul.e.taylor@me.com>
2023-03-21 08:08:28 -0700

    ensure scripts aren't reentrant when sourced (#52)
    * ensure scripts aren't reentrant when sourced
    
    * bump version

--- features/src/utils/opt/devcontainer/bin/post-create-command.sh
+++ features/src/utils/opt/devcontainer/bin/post-create-command.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-for cmd in $(find /opt -type f -name post-create-command.sh ! -wholename $(realpath -m $0)); do
+for cmd in $(find /opt -type f -name post-create-command.sh ! -wholename $(realpath -m "${BASH_SOURCE[0]}")); do
     . $cmd;
 done
