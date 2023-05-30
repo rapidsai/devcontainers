e28472135eda97fc999900f25be6653664b80c8a
ptaylor <paul.e.taylor@me.com>
2023-05-30 14:10:55 -0700

    install gitlab-cli feature, update dev scripts to mount in configs/feature sources

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,26 +1,55 @@
 #! /usr/bin/env bash
 
-# cd to the repo root
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
+run_devcontainer() {
 
-# Generate the devcontainer
-. ./scripts/generate.sh "$@";
+    set -euo pipefail;
 
-env='VAULT_HOST=https://vault.ops.k8s.rapids.ai';
+    # PS4='+ ${LINENO}: '; set -x;
 
-# Start the devcontainer
-devcontainer up \
-    --skip-post-attach \
-    --remote-env "$env" \
-    --remove-existing-container \
-    --workspace-folder "${workspace}" \
-    --cache-from "docker.io/rapidsai/devcontainers:${tag}" \
-    ;
+    # cd to the repo root
+    cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 
-container_id="$(docker ps | grep -P "vsc-${workspace#"${TMPDIR:-/tmp}/"}-[0-9a-z]{64}-uid" | cut -d' ' -f1)";
+    # Generate the devcontainer
+    eval "$(./scripts/generate.sh "$@" | xargs -r -d'\n' -I% echo -n local %\;)";
 
-cmd="${@:3}";
+    mkdir -p -m 0755 .{aws,cache,config};
 
-trap "docker rm -f '${container_id}'" EXIT;
+    local env=();
+    env+=("TERM=${TERM}");
+    env+=("LINES=$(tput lines)");
+    env+=("COLUMNS=$(tput cols)");
+    env+=("SCCACHE_REGION=us-east-2");
+    env+=("SCCACHE_BUCKET=rapids-sccache-devs");
+    env+=("VAULT_HOST=https://vault.ops.k8s.rapids.ai");
+    env+=("HISTFILE=/home/coder/.cache/._bash_history");
 
-docker exec -it -u coder -w /home/coder -e "${env}" "${container_id}" ${cmd:-/bin/bash -li};
+    # Start the devcontainer
+    devcontainer up \
+        --skip-post-attach \
+        $(echo "${env[@]}" | xargs -r -d' ' -I% echo -n '--remote-env % ')\
+        --remove-existing-container \
+        --terminal-rows "$(tput lines)" \
+        --terminal-columns "$(tput cols)" \
+        --workspace-folder "${workspace}" \
+        --mount-workspace-git-root false \
+        --cache-from "docker.io/rapidsai/devcontainers:${tag}" \
+        --mount "type=bind,source=$(pwd)/.aws,target=/home/coder/.aws" \
+        --mount "type=bind,source=$(pwd)/.cache,target=/home/coder/.cache" \
+        --mount "type=bind,source=$(pwd)/.config,target=/home/coder/.config" \
+        --mount "type=bind,source=$(pwd)/features/src/utils/opt/devcontainer,target=/opt/devcontainer" \
+        --additional-features '{ "./features/rapids-build-utils": {} }' \
+        --mount "type=bind,source=$(pwd)/features/src/rapids-build-utils/opt/rapids-build-utils,target=/opt/rapids-build-utils" \
+        ;
+
+    local container_id="$(docker ps | grep -P "vsc-${workspace#"${TMPDIR:-/tmp}/"}-[0-9a-z]{64}-uid" | cut -d' ' -f1)";
+
+    local cmd="${@:3}";
+
+    trap "docker rm -f '${container_id}'" EXIT;
+
+    docker exec -it -u coder -w /home/coder \
+        $(echo "${env[@]}" | xargs -r -d' ' -I% echo -n '-e % ')\
+        "${container_id}" ${cmd:-/bin/bash};
+}
+
+run_devcontainer "$@";
