09f5a8e8fef12084873d9309c04750e0b44a5a01
ptaylor <paul.e.taylor@me.com>
2023-05-30 18:56:09 -0700

    update clone-<lib> template script to use new clone-{github,gitlab}-repo impls

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/clone.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/clone.tmpl.sh
@@ -3,25 +3,35 @@
 clone_${NAME}() {
     set -euo pipefail;
 
-    if [[ ! -d ~/'${SRC_PATH}/.git' ]]; then
+    # PS4='+ ${LINENO}: '; set -x;
 
-        local git_tag='${GIT_TAG}';
-        local git_args="${@}";
-        if ! echo "${git_args}" | grep -qE '(\-b |\-b=|\-\-branch |\-\-branch=)'; then
-            git_args="${git_args:+"$git_args "}${git_tag:+"--branch $git_tag"}";
-        fi
+    if [[ ! -d ~/"${SRC_PATH}"/.git ]]; then
+
+        local branch=;
+
+        eval "$(                                  \
+            devcontainer-utils-parse-args         \
+                --names 'b|branch' "$@"           \
+          | xargs -r -d'\n' -I% echo -n local %\; \
+          # | cat - <(echo 'set -- ${__rest__[@]}') \
+        )";
 
         echo 'Cloning ${NAME}' 1>&2;
 
-        devcontainer-utils-clone-${GIT_HOST}-repo \
-            '${GIT_UPSTREAM}'                     \
-            '${GIT_REPO}'                         \
-            '${SRC_PATH}'                         \
-             ${git_args}                          \
-             --tags                               \
-             --recurse-submodules -j $(nproc)     \
-             -c checkout.defaultRemote=upstream   \
-            ;
+        __rest__+=(--tags);
+        __rest__+=(--recurse-submodules);
+        __rest__+=(-j $(nproc --ignore=2));
+        __rest__+=(-c checkout.defaultRemote=upstream);
+
+        devcontainer-utils-clone-${GIT_HOST}-repo ${__rest__[@]} \
+            --branch "${b:-${branch:-"${GIT_TAG}"}}" \
+            "${GIT_UPSTREAM}/${GIT_REPO}" \
+            ~/"${SRC_PATH}";
+
+        git -C ~/"${SRC_PATH}" config --add remote.upstream.fetch '^refs/heads/pull-request/*';
+        git -C ~/"${SRC_PATH}" branch --remotes -d \
+            $(git -C ~/"${SRC_PATH}" branch --remotes --list 'upstream/pull-request/*');
+        git -C ~/"${SRC_PATH}" remote prune upstream;
 
         rapids-generate-scripts;
 
