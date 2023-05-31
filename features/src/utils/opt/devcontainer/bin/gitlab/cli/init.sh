949935cafbfeee249a756abdc18ca3d01b59734b
ptaylor <paul.e.taylor@me.com>
2023-05-30 18:51:59 -0700

    respect GITLAB_HOST envvar, ensure interactivity is preserved, fix printing missing token warning to stderr

--- features/src/utils/opt/devcontainer/bin/gitlab/cli/init.sh
+++ features/src/utils/opt/devcontainer/bin/gitlab/cli/init.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 _add_warning_to_bashrc() {
-    if [[ "$(grep -q devcontainer-utils-print-missing-gitlab-token-warning ~/.bashrc; echo $?)" == 1 ]]; then
+    if ! grep -q devcontainer-utils-print-missing-gitlab-token-warning ~/.bashrc; then
         echo 'devcontainer-utils-print-missing-gitlab-token-warning;' >> ~/.bashrc;
     fi
 }
 
 _remove_warning_from_bashrc() {
-    if [[ "$(grep -q devcontainer-utils-print-missing-gitlab-token-warning ~/.bashrc; echo $?)" == 0 ]]; then
+    if grep -q devcontainer-utils-print-missing-gitlab-token-warning ~/.bashrc; then
         sed -i 's@devcontainer-utils-print-missing-gitlab-token-warning;@@g' ~/.bashrc;
     fi
 }
@@ -16,40 +16,51 @@ init_gitlab_cli() {
 
     set -euo pipefail;
 
-    if [[ "${CODESPACES:-false}" == true ]]; then
-        if [[ -z "${GITLAB_TOKEN:-}" ]]; then
-            devcontainer-utils-print-missing-gitlab-token-warning;
-            _add_warning_to_bashrc;
-            return;
+    PS4='+ ${LINENO}: '; set -x;
+
+    if [ "${CODESPACES:-false}" = "true" ]; then
+        if [ -z "${GITLAB_TOKEN:-}" ]; then
+            if ! [ -t 0 ] || ! [ -t /dev/tty ]; then
+                devcontainer-utils-print-missing-gitlab-token-warning;
+                _add_warning_to_bashrc;
+                exit 1;
+            fi
         fi
-        glab config set --global git_protocol https;
     fi
 
-    if [[ $(glab auth status 2>&1 | grep "No token provided" >/dev/null 2>&1; echo $?) == 0 ]]; then
-        if [[ -z "${GITLAB_TOKEN:-}" ]]; then
-            glab auth login --hostname gitlab.com;
+    if glab auth status 2>&1 | grep -q "No token provided"; then
+        if [ -z "${GITLAB_TOKEN:-}" ]; then
+            glab auth login --hostname "${GITLAB_HOST:-gitlab.com}" || echo "Continuing without logging into GitLab";
+
+        #     # Theoretically this should work, but glab CLI removes newlines in a way that `script` can't handle
+        #     faketty() { script -qefc "$(printf "%q " "$@")" /dev/null; }
+        #     faketty glab auth login --hostname "${GITLAB_HOST:-gitlab.com}" \
+        #   | sed -u 's@openid profile read_user write_repository api@openid%20profile%20read_user%20write_repository%20api@' \
+        #     `# rewriting redirect_url=http://localhost fails OAuth validation, so we run with --network=host instead`
+        #     `# | sed -u "s@http://localhost@http://$(hostname -i)@"` \
+        #  || echo "Continuing without logging into GitLab";
         else
-            glab auth login  --hostname gitlab.com --stdin < <(echo "${GITLAB_TOKEN}");
+            glab auth login --hostname "${GITLAB_HOST:-gitlab.com}" --stdin < <(echo "${GITLAB_TOKEN}") || echo "Continuing without logging into GitLab";
         fi
     fi
 
     local gitlab_user="${GITLAB_USER:-}";
 
-    if [[ -z "${gitlab_user:-}" ]]; then
-        if [[ -f ~/.config/glab-cli/config.yml ]]; then
+    if [ -z "${gitlab_user:-}" ]; then
+        if [ -f ~/.config/glab-cli/config.yml ]; then
             gitlab_user="$(grep --color=never 'user:' ~/.config/glab-cli/config.yml | cut -d ':' -f2 | tr -d '[:space:]' || echo '')";
         fi
     fi
 
-    if [[ -z "${gitlab_user:-}" ]]; then
-        gitlab_user="$(glab api user | jq -r '.username')";
+    if [ -z "${gitlab_user:-}" ]; then
+        gitlab_user="$(glab api user 2>/dev/null | jq -r '.username // ""')";
     fi
 
-    if [[ -n "${gitlab_user:-}" ]]; then
+    if [ -n "${gitlab_user:-}" ]; then
         _remove_warning_from_bashrc;
     fi
 
-    echo "GITLAB_USER=${gitlab_user}";
+    export GITLAB_USER="${gitlab_user}";
 }
 
-(init_gitlab_cli "$@");
+init_gitlab_cli "$@";
