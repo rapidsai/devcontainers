3982749cc6a0e105240c1ff7f56fe335af690147
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2023-07-12 15:05:30 -0700

    fix cron math (#95)

--- features/src/utils/opt/devcontainer/bin/vault/s3/creds/schedule.sh
+++ features/src/utils/opt/devcontainer/bin/vault/s3/creds/schedule.sh
@@ -5,23 +5,29 @@ schedule_s3_creds_refresh() {
     set -euo pipefail;
 
     local now="$(date '+%s')";
-    local ttl="${VAULT_S3_TTL:-"43200s"}";
+    local ttl="${VAULT_S3_TTL:-"43200"}";
     ttl="${ttl%s}";
+
+    local stamp="$(cat ~/.aws/stamp || echo "${now}")";
+    local then="$((ttl - (now - stamp)))";
+    then="$((then < ttl ? ttl : then))";
+    then="$((((then + 59) / 60) * 60))";
+    then="$((now + then))";
 
     crontab -u $(whoami) -r 2>/dev/null || true;
 
-    if [ -f ~/.aws/stamp ]; then
-        local stamp="$(cat ~/.aws/stamp)";
-        if [ $((now - stamp)) -le ${ttl} ]; then
-            local when="$((now + (ttl - (now - stamp)) - 1))";
-            local expr="$(date --date="@${when}" '+%M %H %d %m %w')";
-            cat <<________EOF | crontab -u $(whoami) -
+    cat <<____EOF | crontab -u $(whoami) -
 SHELL=/bin/bash
-BASH_ENV=/etc/bash.bash_env
-${expr} source \$BASH_ENV && (eval devcontainer-utils-vault-s3-creds-{generate\ \&\&,schedule}) 2>&1 | tee -a /var/log/devcontainer-utils-vault-s3-creds-refresh.log
-________EOF
-        fi
-    fi
+BASH_ENV="${BASH_ENV:-}"
+VAULT_HOST="${VAULT_HOST:-}"
+GITHUB_USER="${GITHUB_USER:-}"
+SCCACHE_BUCKET="${SCCACHE_BUCKET:-}"
+SCCACHE_REGION="${SCCACHE_REGION:-}"
+VAULT_GITHUB_ORGS="${VAULT_GITHUB_ORGS:-}"
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-}"
+$(date --date="@${then}" '+%M %H %d %m %w') \
+bash -lc 'devcontainer-utils-vault-s3-creds-generate && devcontainer-utils-vault-s3-creds-schedule' 2>&1 | tee -a /var/log/devcontainer-utils-vault-s3-creds-refresh.log
+____EOF
 }
 
 if test -n "${devcontainer_utils_debug:-}"; then
--- features/src/utils/opt/devcontainer/bin/vault/s3/creds/test.sh
+++ features/src/utils/opt/devcontainer/bin/vault/s3/creds/test.sh
@@ -11,7 +11,7 @@ test_aws_creds() {
     if test -f ~/.aws/stamp; then
         local now="$(date '+%s')";
         local stamp="$(cat ~/.aws/stamp)";
-        local ttl="${VAULT_S3_TTL:-"43200s"}";
+        local ttl="${VAULT_S3_TTL:-"43200"}";
         if [ $((now - stamp)) -ge "${ttl%s}" ]; then
             exit 1;
         fi
