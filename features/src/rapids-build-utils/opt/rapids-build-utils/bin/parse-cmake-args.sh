2dad4ca72939dcef37194a39ea623dbb3d4862e7
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2023-07-18 07:15:52 -0700

    define default cmake args as envvars (#98)

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/parse-cmake-args.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/parse-cmake-args.sh
@@ -5,15 +5,8 @@ parse_cmake_args() {
     set -euo pipefail;
 
     local ARG="";
-    local args=(${CMAKE_ARGS:-});
-    args+=(-GNinja);
-    args+=(-DCMAKE_BUILD_TYPE=Release);
-    args+=(-DCMAKE_EXPORT_COMPILE_COMMANDS=ON);
-    args+=(-DCMAKE_CUDA_ARCHITECTURES=${CUDAARCHS:-native});
-    args+=(-DCMAKE_C_COMPILER="$(realpath -m "$(which gcc)")");
-    args+=(-DCMAKE_CXX_COMPILER="$(realpath -m "$(which g++)")");
-    args+=(-DCMAKE_CUDA_COMPILER="$(realpath -m "$(which nvcc)")");
-    args+=(-DCMAKE_CUDA_HOST_COMPILER="$(realpath -m "$(which gcc)")");
+    local args=(-GNinja);
+    args+=(${CMAKE_ARGS:-});
 
     while test -n "${1:-}"; do
         ARG="${1:-}";
