fa0532eb4fe36e3230572862a8f3aa08f81e698d
ptaylor <paul.e.taylor@me.com>
2023-07-24 11:42:47 -0700

    reload cron after updating crontab

--- features/src/utils/opt/devcontainer/bin/vault/s3/creds/schedule.sh
+++ features/src/utils/opt/devcontainer/bin/vault/s3/creds/schedule.sh
@@ -8,7 +8,7 @@ schedule_s3_creds_refresh() {
     local ttl="${VAULT_S3_TTL:-"43200"}";
     ttl="${ttl%s}";
 
-    local stamp="$(cat ~/.aws/stamp || echo "${now}")";
+    local stamp="$(cat ~/.aws/stamp 2>/dev/null || echo "${now}")";
     local then="$((ttl - (now - stamp)))";
     then="$((then < ttl ? ttl : then))";
     then="$((((then + 59) / 60) * 60))";
@@ -21,6 +21,7 @@ SHELL=/bin/bash
 BASH_ENV="${BASH_ENV:-}"
 VAULT_HOST="${VAULT_HOST:-}"
 GITHUB_USER="${GITHUB_USER:-}"
+VAULT_S3_TTL="${VAULT_S3_TTL:-}"
 SCCACHE_BUCKET="${SCCACHE_BUCKET:-}"
 SCCACHE_REGION="${SCCACHE_REGION:-}"
 VAULT_GITHUB_ORGS="${VAULT_GITHUB_ORGS:-}"
@@ -28,6 +29,8 @@ AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-}"
 $(date --date="@${then}" '+%M %H %d %m %w') \
 bash -lc 'devcontainer-utils-vault-s3-creds-generate && devcontainer-utils-vault-s3-creds-schedule' 2>&1 | tee -a /var/log/devcontainer-utils-vault-s3-creds-refresh.log
 ____EOF
+
+    /etc/init.d/cron reload >/dev/null 2>&1;
 }
 
 if test -n "${devcontainer_utils_debug:-}"; then
