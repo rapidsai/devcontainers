77832e34a897f8f0a8fab4ba8684c4f4421677c5
Wesley Maxey <wesley.maxey@gmail.com>
2023-07-27 17:58:30 -0700

    Push images to dockerhub additionally

--- .github/actions/push-windows-image/action.ps1
+++ .github/actions/push-windows-image/action.ps1
@@ -12,9 +12,24 @@ Param(
     [string]
     $isolation="hyperv",
     [Parameter(Mandatory=$true)]
-    [string]
-    $repo
+    [alias ("r", "repo")]
+    [string[]]
+    $repos,
+    [switch]
+    [alias ("n")]
+    $dryrun
 )
+
+if ($dryrun) {
+    function Dry-Run {
+        Write-Warning "$args"
+    }
+}
+else {
+    function Dry-Run {
+        Invoke-Expression "$args"
+    }
+}
 
 $ErrorActionPreference = "Stop"
 
@@ -22,10 +37,17 @@ $ErrorActionPreference = "Stop"
 .\scripts\windows\vs-version-matrix.ps1
 $clVerArray = ($vsVerToCompilers[$msvcVersion])
 
+Write-Output "Pushing to repositories: [$repos]"
+$baserepo, $repos = $repos
 
+# Push to the base repo first, then push to other repos
 foreach($cl in $clVerArray) {
-    $image=$(.\scripts\windows\generate-image-name -clVersion $cl -cudaVersion $cudaVersion -edition $edition -repo $repo)
-    Write-Output "Pushing $image"
+    $baseimage=$(.\scripts\windows\generate-image-name -clVersion $cl -cudaVersion $cudaVersion -edition $edition -repo $baserepo)
+    Dry-Run docker push $baseimage
 
-    docker push $image
+    foreach($repo in $repos) {
+        $image=$(.\scripts\windows\generate-image-name -clVersion $cl -cudaVersion $cudaVersion -edition $edition -repo $repo)
+        Dry-Run docker tag $baseimage $image
+        Dry-Run docker push $image
+    }
 }
