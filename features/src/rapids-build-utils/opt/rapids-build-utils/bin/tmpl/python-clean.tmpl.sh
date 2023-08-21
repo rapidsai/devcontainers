66026c3b2a32d942a3d2297320515a117fc169f0
ptaylor <paul.e.taylor@me.com>
2023-08-21 11:25:09 -0700

    fix removing setuptools build dirs

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/python-clean.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/python-clean.tmpl.sh
@@ -6,8 +6,19 @@ clean_${PY_LIB}_cpp() {
 
     local py_lib="$(tr '-' '_' <<< "${PY_LIB}")";
 
-    rm -rf ~/"${PY_SRC}"/{build,_skbuild};
-    rm -rf ~/"${PY_SRC}/${py_lib}.egg-info";
+    rm -rf ~/"${PY_SRC}"/{_skbuild,${py_lib}.egg-info};
+
+    local python_version="${PYTHON_VERSION:-$(python3 --version 2>&1 | cut -d' ' -f2)}";
+    python_version="$(cut -d'.' -f3 --complement <<< "${python_version}")";
+    python_version="${python_version/./}";
+
+    local dir;
+    for dir in lib temp dist; do
+        local slug="${dir}.$(uname -s)-$(uname -m)-cpython-${python_version/./}";
+        if test -d ~/"${PY_SRC}"/build/${slug,,}; then
+            rm -rf ~/"${PY_SRC}"/build/${slug,,};
+        fi
+    done
 
     if test -d ~/"${PY_SRC}/${PY_LIB}"/; then
         find ~/"${PY_SRC}/${PY_LIB}"/ -type f \
