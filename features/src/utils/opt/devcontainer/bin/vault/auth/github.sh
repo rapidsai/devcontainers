7076c9c5ec2af77b08adca42e8238abb6ed2d2ab
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2023-08-21 14:21:01 -0700

    backport fix for case-sensitive org names in vault S3 credential generation (#117)

--- features/src/utils/opt/devcontainer/bin/vault/auth/github.sh
+++ features/src/utils/opt/devcontainer/bin/vault/auth/github.sh
@@ -8,19 +8,24 @@ get_vault_token() {
     local VAULT_HOST="$1";
     local user_orgs="${@:2}";
     local gh_token="$(gh auth token)";
-    local org
 
     local vault_token=null;
+    local org;
 
     for org in ${user_orgs}; do
-        vault_token="$(                                   \
-            curl -s                                       \
-                -X POST                                   \
-                -H "Content-Type: application/json"       \
-                -d "{\"token\": \"$gh_token\"}"           \
-                "$VAULT_HOST/v1/auth/github-${org}/login" \
-          | jq -r '.auth.client_token'                    \
-        )";
+        for org in $(echo -e "${org}\n${org,,}\n${org^^}" | sort -su); do
+            vault_token="$(                                   \
+                curl -s                                       \
+                    -X POST                                   \
+                    -H "Content-Type: application/json"       \
+                    -d "{\"token\": \"$gh_token\"}"           \
+                    "$VAULT_HOST/v1/auth/github-${org}/login" \
+              | jq -r '.auth.client_token'                    \
+            )";
+            if [[ "${vault_token:-null}" != null ]]; then
+                break;
+            fi
+        done
         if [[ "${vault_token:-null}" != null ]]; then
             break;
         fi
@@ -28,5 +33,9 @@ get_vault_token() {
 
     echo "vault_token='$vault_token'";
 }
+
+if test -n "${devcontainer_utils_debug:-}"; then
+    PS4="+ ${BASH_SOURCE[0]}:\${LINENO} "; set -x;
+fi
 
 (get_vault_token "$@");
