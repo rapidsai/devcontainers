925addfdf834d204a63b3f19f2fed2351ff2d143
Christopher Harris <xixonia@gmail.com>
2023-08-23 13:35:33 -0500

    remove comment about caching yq output

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/list-repos.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/list-repos.sh
@@ -46,8 +46,6 @@ ________EOF
 )";
     fi
 
-    # yq is slow, so consider caching the json in a file (/tmp/rapids-manifest.yaml),
-    # using only if tmp file is newer than main manifest
     yq -Mo json "${manifest}" | jq -r "${query}";
 }
 
