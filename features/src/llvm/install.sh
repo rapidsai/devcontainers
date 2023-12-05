6b889f81ecf06dd94b8675ccf11377b73004ae34
Wesley Maxey <wesley.maxey@gmail.com>
2023-12-04 18:25:26 -0800

    Create a 16.grco option for LLVM.

--- features/src/llvm/install.sh
+++ features/src/llvm/install.sh
@@ -22,26 +22,55 @@ check_packages                  \
     software-properties-common  \
     ;
 
-if [[ -z "$LLVM_VERSION" \
-      || "$LLVM_VERSION" == "latest" \
-      || "$LLVM_VERSION" == "dev" \
-      || "$LLVM_VERSION" == "pre" \
-      || "$LLVM_VERSION" == "prerelease" \
-]]; then
-    LLVM_VERSION="latest";
-    find_version_from_git_tags \
-        LLVM_VERSION \
-        https://github.com/llvm/llvm-project \
-        "tags/llvmorg-" "." "-init" "true";
-    LLVM_VERSION="$(echo $LLVM_VERSION | grep -oP '[0-9]+')";
+install_clang_grace() {
+    if [[ "$(uname -p)" != "aarch64" ]]; then
+        echo "Clang for Grace is only supported on aarch64"
+        exit 0;
+    fi
+
+    echo "Installing NVIDIA Clang for Grace"
+    wget -O clang-grace-toolchain.deb https://developer.nvidia.com/downloads/assets/grace/clang/16.23.08/clang-grace-toolchain_16.23.08_arm64.deb
+    apt-get install ./clang-grace-toolchain
+
+    PATH="/opt/nvidia/clang/16.23.08/bin:$PATH"
+    (update-alternatives --install /usr/bin/clang clang $(which clang) 30);
+    (update-alternatives --install /usr/bin/clang-16 clang-16 $(which clang) 30);
+    (update-alternatives --install /usr/bin/clang++ clang $(which clang++) 30);
+    (update-alternatives --install /usr/bin/clang++-16 clang++-16 $(which clang++) 30);
+}
+
+install_llvm() {
+    if [[ -z "$LLVM_VERSION" \
+        || "$LLVM_VERSION" == "latest" \
+        || "$LLVM_VERSION" == "dev" \
+        || "$LLVM_VERSION" == "pre" \
+        || "$LLVM_VERSION" == "prerelease" \
+    ]]; then
+        LLVM_VERSION="latest";
+        find_version_from_git_tags \
+            LLVM_VERSION \
+            https://github.com/llvm/llvm-project \
+            "tags/llvmorg-" "." "-init" "true";
+        LLVM_VERSION="$(echo $LLVM_VERSION | grep -oP '[0-9]+')";
+    fi
+
+    echo "Installing llvm-${LLVM_VERSION} compilers and tools";
+
+    ./llvm.sh $LLVM_VERSION ${PACKAGES:-all};
+}
+
+if [[ "$LLVM_VERSION" == "16.grco" ]]; then
+    LLVM_VERSION="16"
+    install_llvm
+    install_clang_grace
+    LLVM_TOOLS=( clangd clang-format clang-tidy )
+else
+    install_llvm
+    LLVM_TOOLS=( clang clangd clang++ clang-format clang-tidy lldb llvm-config llvm-cov )
 fi
 
-echo "Installing llvm-${LLVM_VERSION} compilers and tools";
-
-./llvm.sh $LLVM_VERSION ${PACKAGES:-all};
-
 # Remove existing, install, and set default clang/llvm alternatives
-for x in clang clangd clang++ clang-format clang-tidy lldb llvm-config llvm-cov; do
+for x in ${LLVM_TOOLS[@]}; do
     if type ${x}-${LLVM_VERSION} >/dev/null 2>&1; then
         if ${x} != clang-format; then
             (update-alternatives --remove-all ${x} >/dev/null 2>&1 || true);
