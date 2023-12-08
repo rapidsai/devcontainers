377ba6cde0df18cffa96c2a3bddb98f93d14c1a8
Bradley Dice <bdice@bradleydice.com>
2023-12-08 14:12:43 -0800

    Remove index.

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-attach-command.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-attach-command.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/env bash
 
 if test -n "${PYTHON_PACKAGE_MANAGER:-}"; then
-    if [ "${PYTHON_PACKAGE_MANAGER:-}" = "pip" ] \
-    && devcontainer-utils-vault-s3-creds-test 2>&1 >/dev/null \
-    && ! grep -q 'pypi.anaconda.org' <<< "$(pip config get global.extra-index-url 2>&1)"; then
-        pip config set \
-            "global.extra-index-url" \
-            "https://pypi.anaconda.org/rapidsai-wheels-nightly/simple";
-    fi
-
     rapids-make-${PYTHON_PACKAGE_MANAGER}-env || true;
 fi
