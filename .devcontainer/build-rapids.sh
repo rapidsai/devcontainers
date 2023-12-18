e72eeeee01d2e170887a2824570e6563993a72e7
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2023-12-18 15:01:45 -0800

    Add wholegraph to `manifest.yaml` (#191)
    * add wholegraph to manifest.yaml
    
    * bump rapids-build-utils version
    
    * add wholegraph to unified devcontainers
    
    * update build-rapids.sh

--- .devcontainer/build-rapids.sh
+++ .devcontainer/build-rapids.sh
@@ -26,72 +26,71 @@ build_rapids() {
     (
         echo "building RMM";
         clean-rmm;
-        build-rmm -DBUILD_BENCHMARKS=ON;
+        build-rmm -DBUILD_BENCHMARKS=ON --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log rmm;
 
     (
         echo "building KvikIO";
         clean-kvikio;
-        build-kvikio-cpp;
-        build-kvikio-python;
+        build-kvikio --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log kvikio;
 
     (
         echo "building cuDF";
         clean-cudf;
-    CUDF_ROOT=~/cudf/cpp/build/latest \
-    CUDF_INCLUDE_DIR=~/cudf/cpp/include \
-    CUDF_KAFKA_ROOT=~/cudf/cpp/libcudf_kafka/build/latest \
-    CUDF_KAFKA_INCLUDE_DIR=~/cudf/cpp/libcudf_kafka/include \
-    CUDA_HOME="${CONDA_PREFIX:-${CUDA_HOME:-/usr/local/cuda}}" \
-    C_INCLUDE_PATH=${C_INCLUDE_PATH:-}:~/rmm/include:~/rmm/build/latest/include \
-    CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH:-}:~/rmm/include:~/rmm/build/latest/include \
-        build-cudf -DBUILD_BENCHMARKS=ON;
+        build-cudf -DBUILD_BENCHMARKS=ON -DNVBench_ENABLE_CUPTI=OFF --verbose
         sccache -s;
     ) 2>&1 | maybe_write_build_log cudf;
 
     (
         echo "building RAFT";
         clean-raft;
-        build-raft -DBUILD_PRIMS_BENCH=ON -DBUILD_ANN_BENCH=ON;
+        build-raft -DBUILD_PRIMS_BENCH=ON -DBUILD_ANN_BENCH=ON --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log raft;
 
     (
         echo "building cuMLPrims";
         clean-cumlprims_mg;
-        build-cumlprims_mg;
+        build-cumlprims_mg --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log cumlprims_mg;
 
     (
         echo "building cuML";
         clean-cuml;
-        build-cuml;
+        build-cuml --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log cuml;
 
     (
         echo "building cuGraph OPS";
         clean-cugraph-ops;
-        build-cugraph-ops;
+        build-cugraph-ops --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log cugraph-ops;
+
+    (
+        echo "building wholegraph";
+        clean-wholegraph;
+        build-wholegraph --verbose;
+        sccache -s;
+    ) 2>&1 | maybe_write_build_log wholegraph;
 
     (
         echo "building cuGraph";
         clean-cugraph;
     CUDA_HOME="${CONDA_PREFIX:-${CUDA_HOME:-/usr/local/cuda}}" \
-        build-cugraph --max-device-obj-memory-usage 5;
+        build-cugraph --verbose --max-device-obj-memory-usage 5;
         sccache -s;
     ) 2>&1 | maybe_write_build_log cugraph;
 
     (
         echo "building cuSpatial";
         clean-cuspatial;
-        build-cuspatial -DBUILD_TESTS=ON -DBUILD_BENCHMARKS=ON;
+        build-cuspatial -DBUILD_TESTS=ON -DBUILD_BENCHMARKS=ON --verbose;
         sccache -s;
     ) 2>&1 | maybe_write_build_log cuspatial;
 
