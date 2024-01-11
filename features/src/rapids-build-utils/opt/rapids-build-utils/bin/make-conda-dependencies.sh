ddefcce075962140506fd3887136035205ed4f6c
ptaylor <paul.e.taylor@me.com>
2024-01-11 12:25:23 -0800

    generate dependencies per python lib + file_key

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-conda-dependencies.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-conda-dependencies.sh
@@ -24,11 +24,10 @@ make_conda_dependencies() {
     local python_version="${PYTHON_VERSION:-$(python3 --version 2>&1 | cut -d' ' -f2)}";
     python_version="$(cut -d'.' -f3 --complement <<< "${python_version}")";
 
-    local conda_noinstall=();
     local conda_env_yamls=();
 
     eval "$(                                  \
-        rapids-list-repos "$@"                \
+        rapids-list-repos ${__rest__[@]}      \
       | xargs -r -d'\n' -I% echo -n local %\; \
     )";
 
@@ -41,34 +40,39 @@ make_conda_dependencies() {
         local repo_path="${repo}_path";
 
         if [ -f ~/"${!repo_path}/dependencies.yaml" ]; then
-            conda_noinstall+=("$(rapids-python-conda-pkg-names --repo "${!repo_name}")");
 
-            echo "Generating ${!repo_name}'s conda env yml" 1>&2;
+            local py_length="${repo}_python_length";
 
-            local repo_keys=(${keys[@]} ${keys[@]/%/_${!repo_name}});
-            local keyi;
+            for ((j=0; j < ${!py_length:-0}; j+=1)); do
+                local py_name="${repo}_python_${j}_name";
 
-            for ((keyi=0; keyi < ${#repo_keys[@]}; keyi+=1)); do
-                if rapids-dependency-file-generator                                       \
-                    --file_key ${repo_keys[$keyi]}                                        \
-                    --output conda                                                        \
-                    --config ~/"${!repo_path}/dependencies.yaml"                          \
-                    --matrix "arch=$(uname -m);cuda=${cuda_version};py=${python_version}" \
-                >> /tmp/${!repo_name}${keyi}.yaml 2>/dev/null; then
-                    conda_env_yamls+=("/tmp/${!repo_name}${keyi}.yaml");
-                fi
+                echo "Generating ${!py_name}'s conda env yml" 1>&2;
+
+                local repo_keys=(${keys[@]} ${keys[@]/%/_${!py_name//"-"/"_"}});
+                local keyi;
+
+                for ((keyi=0; keyi < ${#repo_keys[@]}; keyi+=1)); do
+                    if rapids-dependency-file-generator                                       \
+                        --file_key ${repo_keys[$keyi]}                                        \
+                        --output conda                                                        \
+                        --config ~/"${!repo_path}/dependencies.yaml"                          \
+                        --matrix "arch=$(uname -m);cuda=${cuda_version};py=${python_version}" \
+                        `# --stdout` \
+                    >> /tmp/${!repo_name}.${!py_name}.${keyi}.env.yaml 2>/dev/null; then
+                        conda_env_yamls+=("/tmp/${!repo_name}.${!py_name}.${keyi}.env.yaml");
+                    fi
+                done
             done
         fi
     done
 
     if test ${#conda_env_yamls[@]} -gt 0; then
-
+        local conda_noinstall=($(rapids-python-pkg-names) $(rapids-python-conda-pkg-names));
         # Generate a combined conda env yaml file.
-        conda-merge ${conda_env_yamls[@]}                             \
-          | grep -v '^name:'                                          \
-          | grep -v -P '^(.*?)\-(.*?)rapids-(.*?)$'                   \
-          | grep -v -P '^(.*?)\-(.*?)(\.git\@[^(main|master)])(.*?)$' \
-          | grep -v -P "^(.*?)\-(.*?)($(rapids-join-strings "|" ${conda_noinstall[@]}))(=.*|>.*|<.*)?$" \
+        conda-merge ${conda_env_yamls[@]}                                                                \
+          | grep -v '^name:'                                                                             \
+          | grep -v -P '^[ ]*?\- (\.git\@[^(main|master)])(.*?)$'                                        \
+          | grep -v -P "^[ ]*?\- ($(tr -d '[:blank:]' <<< "${conda_noinstall[@]/%/ |}"))(=.*|>.*|<.*)?$" \
           ;
 
         rm ${conda_env_yamls[@]};
--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-pip-dependencies.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-pip-dependencies.sh
@@ -19,17 +19,20 @@ make_pip_dependencies() {
     fi
 
     local cuda_version="${CUDA_VERSION:-${CUDA_VERSION_MAJOR:-12}.${CUDA_VERSION_MINOR:-0}}";
-    cuda_version="$(grep -o '^[0-9]*.[0-9]' <<< "${cuda_version}")";
+    cuda_version="$(grep -o '^[0-9]*.[0-9]*.[0-9]' <<< "${cuda_version}")";
     local cuda_version_major="$(cut -d'.' -f1 <<< "${cuda_version}")";
+    local cuda_version_minor="$(cut -d'.' -f2 <<< "${cuda_version}")";
+    local cuda_version_patch="$(cut -d'.' -f3 <<< "${cuda_version}")";
+    local cuda_version_mm="${cuda_version_major}.${cuda_version_minor:-0}";
+    cuda_version="${cuda_version_mm}.${cuda_version_patch:-0}";
 
     local python_version="${PYTHON_VERSION:-$(python3 --version 2>&1 | cut -d' ' -f2)}";
     python_version="$(cut -d'.' -f3 --complement <<< "${python_version}")";
 
-    local pip_noinstall=();
     local pip_reqs_txts=();
 
     eval "$(                                  \
-        rapids-list-repos "$@"                \
+        rapids-list-repos ${__rest__[@]}      \
       | xargs -r -d'\n' -I% echo -n local %\; \
     )";
 
@@ -41,48 +44,55 @@ make_pip_dependencies() {
         local repo_name="${repo}_name";
         local repo_path="${repo}_path";
 
-        pip_noinstall+=("lib${!repo_name}" "${!repo_name}");
-
         if [ -f ~/"${!repo_path}/dependencies.yaml" ]; then
-            pip_reqs_txts+=("/tmp/${!repo_name}.requirements.txt");
 
-            echo "Generating ${!repo_name}'s requirements.txt" 1>&2;
+            local py_length="${repo}_python_length";
 
-            local repo_keys=(${keys[@]} ${keys[@]/%/_${!repo_name}});
-            local keyi;
+            for ((j=0; j < ${!py_length:-0}; j+=1)); do
+                local py_name="${repo}_python_${j}_name";
 
-            for ((keyi=0; keyi < ${#repo_keys[@]}; keyi+=1)); do
-                rapids-dependency-file-generator                                          \
-                    --file_key ${repo_keys[$keyi]}                                        \
-                    --output requirements                                                 \
-                    --config ~/"${!repo_path}/dependencies.yaml"                          \
-                    --matrix "arch=$(uname -m);cuda=${cuda_version};py=${python_version}" \
-                    `# --stdout` \
-                >> /tmp/${!repo_name}.requirements.txt 2>/dev/null || true;
+                echo "Generating ${!py_name}'s requirements.txt" 1>&2;
+
+                local repo_keys=(${keys[@]} ${keys[@]/%/_${!py_name//"-"/"_"}});
+                local keyi;
+
+                for ((keyi=0; keyi < ${#repo_keys[@]}; keyi+=1)); do
+                    if rapids-dependency-file-generator                                          \
+                        --file_key ${repo_keys[$keyi]}                                           \
+                        --output requirements                                                    \
+                        --config ~/"${!repo_path}/dependencies.yaml"                             \
+                        --matrix "arch=$(uname -m);cuda=${cuda_version_mm};py=${python_version}" \
+                        `# --stdout` \
+                    >> /tmp/${!repo_name}.${!py_name}.${keyi}.requirements.txt 2>/dev/null; then
+                        pip_reqs_txts+=("/tmp/${!repo_name}.${!py_name}.${keyi}.requirements.txt");
+                    fi
+                done
             done
         fi
     done
 
-    # add all python packages to the noinstall list.
-    for pkg in $(rapids-python-pkg-names); do
-        pip_noinstall+=("${pkg}" "${pkg}-cu.*");
-        if test -z "${pkg##*"-"*}"; then
-            pip_noinstall+=("${pkg//"-"/"_"}" "${pkg//"-"/"_"}-cu.*")
-        fi
-        if test -z "${pkg##*"_"*}"; then
-            pip_noinstall+=("${pkg//"_"/"-"}" "${pkg//"_"/"-"}-cu.*")
-        fi
-    done
-
     if test ${#pip_reqs_txts[@]} -gt 0; then
-        pip_noinstall=($(echo -n "${pip_noinstall[@]}" | xargs -r -n1 -d' ' | sort -su | tr '\n' ' '));
+
+        local pip_noinstall=();
+
+        # add all python packages to the noinstall list.
+        for pkg in $(rapids-python-pkg-names); do
+            pip_noinstall+=("${pkg}" "${pkg}-cu.*");
+            if test -z "${pkg##*"-"*}"; then
+                pip_noinstall+=("${pkg//"-"/"_"}" "${pkg//"-"/"_"}-cu.*")
+            fi
+            if test -z "${pkg##*"_"*}"; then
+                pip_noinstall+=("${pkg//"_"/"-"}" "${pkg//"_"/"-"}-cu.*")
+            fi
+        done
+
         # Generate a combined requirements.txt file
-        cat ${pip_reqs_txts[@]} \
-          | grep -v '^#' \
-          | sed -E "s/-cu([0-9]+)/-cu${cuda_version_major}/g" \
-          | sed -E "s/cupy-cuda[0-9]+x[ ]+?>=.*/cupy-cuda${cuda_version_major}x>=${cuda_version_major}/g" \
-          | sed -E "s/cuda-python[ ]+?>=.*/cuda-python>=${cuda_version_major},<$((cuda_version_major+1)).0a0/g" \
-          | grep -v -P "^($(rapids-join-strings "|" ${pip_noinstall[@]}))" \
+        cat ${pip_reqs_txts[@]}                                                                           \
+          | grep -v '^#'                                                                                  \
+          | grep -v -P "^($(tr -d '[:blank:]' <<< "${pip_noinstall[@]/%/|}"))(=.*|>.*|<.*)?$"             \
+          | sed -E "s/-cu([0-9]+)/-cu${cuda_version_major}/g"                                             \
+          | sed -E "s/cupy-cuda[0-9]+x[ ]+?>=.*/cupy-cuda${cuda_version_major}x>=${cuda_version}/g"       \
+          | sed -E "s/cuda-python[ ]+?>=.*/cuda-python>=${cuda_version},<$((cuda_version_major+1)).0a0/g" \
         ;
 
         rm ${pip_reqs_txts[@]};
