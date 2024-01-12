dfc977f940b9f13dbfade684ffbfba53fe0b7121
ptaylor <paul.e.taylor@me.com>
2024-01-12 11:43:36 -0800

    intercept the --mode param and only pass it to the python build script

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/build.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/build.tmpl.sh
@@ -2,14 +2,27 @@
 
 build_${NAME}() {
     set -euo pipefail;
+
+    local mode="editable";
+
+    # pull out the --mode editable|wheel arg (if any)
+    eval "$(                                  \
+        devcontainer-utils-parse-args --names '
+            m|mode                            |
+        ' - <<< "$@"                          \
+      | xargs -r -d'\n' -I% echo -n local %\; \
+    )";
+
+    mode="${m:-${mode:-"editable"}}";
+
     for lib in ${CPP_LIB}; do
         if type build-${lib}-cpp >/dev/null 2>&1; then
-            build-${lib}-cpp "$@";
+            build-${lib}-cpp ${__rest__[@]};
         fi
     done
     for lib in ${PY_LIB}; do
         if type build-${lib}-python >/dev/null 2>&1; then
-            build-${lib}-python "$@";
+            build-${lib}-python -m ${mode} ${__rest__[@]};
         fi
     done
 }
