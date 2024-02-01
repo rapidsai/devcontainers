8d6d18df019291dc628ecc47c9da56d1709cd781
ptaylor <paul.e.taylor@me.com>
2024-02-01 01:07:24 -0800

    allow cpack'ing components by doing a dummy install and tar'ing everything up ourselves

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/cpp.cpack.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/cpp.cpack.tmpl.sh
@@ -6,12 +6,17 @@
 # CPack ${CPP_LIB}.
 #
 # Boolean options:
-#  -h,--help,--usage            print this text
-#  -v,--verbose                 verbose output
+#  -h,--help,--usage                            print this text
+#  -v,--verbose                                 verbose output
 #
 # Options that require values:
-#  -o,--out-dir <dir>           copy cpack'd TGZ file into <dir>
-#                               (default: none)
+#  --component <comp>                           Component-based install. Only install component <comp>.
+#                                               (default: all)
+#  --config    <cfg>                            For multi-configuration generators, choose configuration <cfg>
+#                                               (default: none)
+#  --default-directory-permissions <permission> Default install permission. Use default permission <permission>.
+#  -o,--out-dir <dir>                           copy cpack'd TGZ file into <dir>
+#                                               (default: none)
 
 cpack_${CPP_LIB}_cpp() {
     local -;
@@ -25,20 +30,76 @@ cpack_${CPP_LIB}_cpp() {
         PS4="+ ${BASH_SOURCE[0]}:\${LINENO} "; set -x;
     fi
 
-    eval "$(devcontainer-utils-parse-args "$0" - <<< "${@@Q}")";
+    eval "$(devcontainer-utils-parse-args "$0" --passthrough '
+        -v,--verbose
+        --strip
+        --config
+        --default-directory-permissions
+    ' - <<< "${@@Q}")";
+
+    test ${#component[@]} -eq 0 && component=(all);
 
     time (
-        (
-            cd "${CPP_SRC}"/build/latest/;
-            cpack -G TGZ ${v:+-V};
-        ) || true;
+        local comp;
+        local name;
+        local slug;
+        local outd;
+        local vers;
+        local kernel;
 
-        if test -d "${out_dir}"/; then
-            find "${CPP_SRC}"/build/latest/     \
-                -iname "${CPP_LIB}-*.tar.gz"    \
-                -print0                         \
-          | xargs -0 -I% cp -a "%" "${out_dir}"/;
+        kernel="$(uname -s)";
+
+        if test -f "${CPP_SRC}"/build/latest/CPackConfig.cmake; then
+            name="$(grep -e '^set(CPACK_PACKAGE_NAME ".*")$' "${CPP_SRC}"/build/latest/CPackConfig.cmake | head -n1)";
+            vers="$(grep -e '^set(CPACK_PACKAGE_VERSION ".*")$' "${CPP_SRC}"/build/latest/CPackConfig.cmake | head -n1)";
+            name="${name#*\"}"; name="${name%\"*}";
+            vers="${vers#*\"}"; vers="${vers%\"*}";
+        elif test -f "${CPP_SRC}"/build/latest/CPackSourceConfig.cmake; then
+            name="$(grep -e '^set(CPACK_PACKAGE_NAME ".*")$' "${CPP_SRC}"/build/latest/CPackSourceConfig.cmake | head -n1)";
+            vers="$(grep -e '^set(CPACK_PACKAGE_VERSION ".*")$' "${CPP_SRC}"/build/latest/CPackSourceConfig.cmake | head -n1)";
+            name="${name#*\"}"; name="${name%\"*}";
+            vers="${vers#*\"}"; vers="${vers%\"*}";
+        else
+            if grep -qe '^CMAKE_PROJECT_NAME:.*=.*$' "${CPP_SRC}"/build/latest/CMakeCache.txt; then
+                name="$(grep -e '^CMAKE_PROJECT_NAME:.*=.*$' "${CPP_SRC}"/build/latest/CMakeCache.txt | head -n1)";
+                name="${name##*=}";
+            fi
+            if grep -qe '^CMAKE_PROJECT_VERSION:.*=.*$' "${CPP_SRC}"/build/latest/CMakeCache.txt; then
+                vers="$(grep -e '^CMAKE_PROJECT_VERSION:.*=.*$' "${CPP_SRC}"/build/latest/CMakeCache.txt | head -n1)";
+                vers="${vers##*=}";
+            elif test -f "${CPP_SRC}"/build/latest/${CPP_LIB}ConfigVersion.cmake; then
+                vers="$(grep -e '^set(PACKAGE_VERSION ".*")$' "${CPP_SRC}"/build/latest/${CPP_LIB}ConfigVersion.cmake | head -n1)";
+                vers="${vers#*\"}"; vers="${vers%\"*}";
+            elif test -f "${CPP_SRC}"/build/latest/${CPP_LIB}-config-version.cmake; then
+                vers="$(grep -e '^set(PACKAGE_VERSION ".*")$' "${CPP_SRC}"/build/latest/${CPP_LIB}-config-version.cmake | head -n1)";
+                vers="${vers#*\"}"; vers="${vers%\"*}";
+            fi
         fi
+
+        if test -z "${name:-}" || test -z "${vers:-}"; then
+            exit 1;
+        fi
+
+        for comp in "${component[@]}"; do
+
+            if test "all" = "${comp}";
+                then comp="";
+            fi
+
+            slug="${name}-${vers}${comp:+-$comp}-${kernel}";
+            outd="${CPP_SRC}/build/latest/_CPack_Packages/${kernel}/TGZ";
+
+            install-${CPP_LIB}-cpp -p "${outd}/${slug}" ${comp:+--component "${comp}"} "${OPTS[@]}";
+
+            if test -d "${outd}/${slug}"; then
+                tar -C "${outd}" -cz ${v:+-v} -f "${outd}/${slug}.tar.gz" "${slug}";
+                cp -a "${outd}/${slug}.tar.gz" "${CPP_SRC}/build/latest/";
+                if test -d "${out_dir}"/ \
+                && test -f "${CPP_SRC}/build/latest/${slug}.tar.gz"; then
+                    cp -a "${CPP_SRC}/build/latest/${slug}.tar.gz" "${out_dir}"/;
+                fi
+            fi
+        done
 
         { set +x; } 2>/dev/null; echo -n "lib${CPP_LIB} CPack time:";
     ) 2>&1;
