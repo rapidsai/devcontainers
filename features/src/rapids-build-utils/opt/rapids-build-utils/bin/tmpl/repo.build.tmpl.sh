34a73c6b1c0da44da36e81067654c1ea746eab02
ptaylor <paul.e.taylor@me.com>
2024-02-01 21:02:40 -0800

    remove pip args from docstring

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/repo.build.tmpl.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/tmpl/repo.build.tmpl.sh
@@ -8,20 +8,6 @@
 # Boolean options:
 #  -h,--help                              print this text
 #  -v,--verbose                           verbose output
-#  --ignore-requires-python               Ignore the Requires-Python information.
-#  --no-clean                             Don't clean up build directories.
-#  --no-deps                              Don't install package dependencies.
-#  --no-verify                            Don't verify if built wheel is valid.
-#  --no-cache-dir                         Disable the pip cache.
-#  --use-pep517                           Use PEP 517 for building source distributions.
-#  --no-use-pep517                        Don't use PEP 517 for building source distributions
-#  --no-build-isolation                   Disable isolation when building a modern source distribution. Build
-#                                         dependencies specified by PEP 518 must be already installed if this option is
-#                                         used.
-#  --pre                                  Include pre-release and development versions. By default, pip only finds
-#                                         stable versions.
-#  --prefer-binary                        Prefer binary packages over source packages, even if the source packages are
-#                                         newer.
 #
 # Options that require values:
 #  -t,--type (editable|wheel)             The type of Python build to run (editable or wheel)
@@ -37,14 +23,6 @@
 #                                         Higher values yield fewer parallel CUDA device object compilations.
 #                                         (default: 1)
 #  -D* <var>[:<type>]=<value>             Create or update a cmake cache entry.
-#  --cache-dir <dir>                      Store the cache data in <dir>.
-#  --only-binary <format_control>         Do not use source packages. Can be supplied multiple times, and each time
-#                                         adds to the existing value. Accepts either ":all:" to disable all source
-#                                         packages, ":none:" to empty the set, or one or more package names with commas
-#                                         between them. Packages without binary distributions will fail to install when
-#                                         this option is used on them.
-#  -w,--wheel-dir <dir>                   copy built wheel into <dir>
-#                                         (default: none)
 
 build_${NAME}() {
     local -;
