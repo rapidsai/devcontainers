183bdc470da11370f860daeba80b4d44ed14cc08
ptaylor <paul.e.taylor@me.com>
2024-02-02 23:08:22 -0800

    use bash-completion-lib to complete directories and filenames

--- features/src/utils/opt/devcontainer/bin/bash/completion.tmpl.sh
+++ features/src/utils/opt/devcontainer/bin/bash/completion.tmpl.sh
@@ -62,42 +62,42 @@ _devcontainer_utils_completions() {
         option_names["${CMD}"]="${bool_names[${CMD}]} ${value_names[${CMD}]}";
     fi
 
+    local cur="${COMP_WORDS[$COMP_CWORD]}";
+    local prev="${COMP_WORDS[$COMP_CWORD-1]}";
+
     if test "${COMP_CWORD}" -gt 1; then
         while read -r name_and_type; do
             local -a pair;
             read -a pair -r <<< "${name_and_type}";
             local name_="${pair[0]}";
             local type_="${pair[1]}";
-            if [[ ${name_} == ${COMP_WORDS[$COMP_CWORD-1]}* ]]; then
+            if [[ ${name_} == ${prev}* ]]; then
                 case "${type_}" in
                     "<num>")
                         # shellcheck disable=SC2207
-                        COMPREPLY=($(grep -P "^[0-9]+$" <<< "${COMP_WORDS[$COMP_CWORD]}"));
+                        COMPREPLY=($(grep -P "^[0-9]+$" <<< "${cur}"));
                         return;
                         ;;
                     "<dir>")
-                        # shellcheck disable=SC2207
-                        COMPREPLY=($(compgen -o nosort -o dirnames -- "${COMP_WORDS[$COMP_CWORD]}"));
+                        _filedir -d;
                         return;
                         ;;
                     "<file>")
-                        # shellcheck disable=SC2207
-                        COMPREPLY=($(compgen -o nosort -o filenames -- "${COMP_WORDS[$COMP_CWORD]}"));
+                        _filedir;
                         return;
                         ;;
                     "<path>")
-                        # shellcheck disable=SC2207
-                        COMPREPLY=($(compgen -o nosort -o default -- "${COMP_WORDS[$COMP_CWORD]}"));
+                        _filedir;
                         return;
                         ;;
                     \(*\))
                         local -a list=();
                         IFS=$'|' read -a list -r <<< "${type_:1:-1}";
-                        if test ${#COMP_WORDS[$COMP_CWORD]} -eq 0; then
+                        if test ${#cur} -eq 0; then
                             COMPREPLY=("${list[@]}");
                         else
                             for item in "${list[@]}"; do
-                                if [[ ${item} =~ ^${COMP_WORDS[$COMP_CWORD]} ]]; then
+                                if [[ ${item} =~ ^${cur} ]]; then
                                     COMPREPLY+=("${item}");
                                 fi
                             done
@@ -116,9 +116,9 @@ _devcontainer_utils_completions() {
     declare -a bools="(${bool_names[${CMD}]})";
 
     for ((idx_a=0; idx_a < ${#bools[@]}; idx_a+=1)); do
-        if [[ ${bools[$idx_a]} == ${COMP_WORDS[$COMP_CWORD]}* ]]; then
+        if [[ ${bools[$idx_a]} == ${cur}* ]]; then
             # shellcheck disable=SC2207
-            COMPREPLY=($(compgen -o nosort -W "${option_names[$CMD]}" -- "${COMP_WORDS[$COMP_CWORD]}"));
+            COMPREPLY=($(compgen -o nosort -W "${option_names[$CMD]}" -- "${cur}"));
             return;
         fi
     done
@@ -126,9 +126,9 @@ _devcontainer_utils_completions() {
     declare -a value="(${value_names[${CMD}]})";
 
     for ((idx_a=0; idx_a < ${#value[@]}; idx_a+=1)); do
-        if [[ ${value[$idx_a]} == ${COMP_WORDS[$COMP_CWORD]}* ]]; then
+        if [[ ${value[$idx_a]} == ${cur}* ]]; then
             # shellcheck disable=SC2207
-            COMPREPLY=($(compgen -o nosort -W "${option_names[$CMD]}" -- "${COMP_WORDS[$COMP_CWORD]}"));
+            COMPREPLY=($(compgen -o nosort -W "${option_names[$CMD]}" -- "${cur}"));
             return;
         fi
     done
