b89951bc4aaf67ef47e9d4643b9d8fc482aeed9b
ptaylor <paul.e.taylor@me.com>
2024-02-27 23:43:01 -0800

    randomize sccache_server_port on container creation

--- features/src/utils/opt/devcontainer/bin/post-create-command.sh
+++ features/src/utils/opt/devcontainer/bin/post-create-command.sh
@@ -7,13 +7,14 @@ if ! test -f /var/run/crond.pid; then
     cron 2>/dev/null || true;
 fi
 
+. "$(dirname "$(realpath -m "${BASH_SOURCE[0]}")")/update-envvars.sh";
+
 # If SSH_AUTH_SOCK exists and isn't owned by the coder user, use socat to proxy
 # between a socket owned by the coder user and the one mounted in from the host
 if test -n "${SSH_AUTH_SOCK:-}" \
 && test "${SSH_AUTH_SOCK}" != ~/.ssh/socket \
 && test "$(stat -c "%u:%g" "${SSH_AUTH_SOCK}")" != "$(id -u):$(id -g)"; then
     # shellcheck disable=SC1091
-    . "$(dirname "$(realpath -m "${BASH_SOURCE[0]}")")/update-envvars.sh";
     reset_envvar SSH_AUTH_SOCK;
     ssh_auth_sock=~/.ssh/socket;
     # If the remapped ssh socket doesn't already exist
@@ -28,4 +29,9 @@ if test -n "${SSH_AUTH_SOCK:-}" \
     export_envvar SSH_AUTH_SOCK "${ssh_auth_sock}";
     export SSH_AUTH_SOCK="${ssh_auth_sock}";
     unset ssh_auth_sock;
+fi
+
+# Randomize the sccache server port in case the container is launched with --network=host
+if test -z "${SCCACHE_SERVER_PORT:-}"; then
+    export_envvar SCCACHE_SERVER_PORT "$((4220 + $RANDOM % 4999))";
 fi
