b648d1f3205dc535c778b939bb4039928f8b4a0e
Lawrence Mitchell <lmitchell@nvidia.com>
2025-02-10 09:52:11 +0000

    Fix path in mambaforge install

--- features/src/mambaforge/install.sh
+++ features/src/mambaforge/install.sh
@@ -58,11 +58,11 @@ cat <<"EOF" > /opt/conda/etc/profile.d/mamba.sh
 if [ -z "${MAMBA_ROOT_PREFIX:-}" ]; then
     export MAMBA_ROOT_PREFIX="${CONDA_PREFIX:-/opt/conda}"
 fi
-__mamba_setup="$("/opt/conda/mamba" shell hook --shell posix 2> /dev/null)"
+__mamba_setup="$("/opt/conda/bin/mamba" shell hook --shell posix 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
 else
-    alias mamba="/opt/conda/mamba"  # Fallback on help from mamba activate
+    alias mamba="/opt/conda/bin/mamba"  # Fallback on help from mamba activate
 fi
 unset __mamba_setup
 EOF
