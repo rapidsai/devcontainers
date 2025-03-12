fae7b984c7fde5dee7eb4044211c84d35862f09a
Allison Piper <alliepiper16@gmail.com>
2025-03-12 16:30:42 -0400

    Update windows CMake version to 3.31.6. (#473)
    Required as part of updating nvbench's CI, since rapids-cmake requires
    at least 3.30.

--- windows/image/installers/install-cmake.ps1
+++ windows/image/installers/install-cmake.ps1
@@ -1,5 +1,5 @@
 # Fetch CMake
-Invoke-WebRequest -Uri "https://github.com/Kitware/CMake/releases/download/v3.27.6/cmake-3.27.6-windows-x86_64.msi" -OutFile "C:\cmake_installer.msi" -UseBasicParsing
+Invoke-WebRequest -Uri "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-windows-x86_64.msi" -OutFile "C:\cmake_installer.msi" -UseBasicParsing
 Start-Process -NoNewWindow -Wait -FilePath msiexec -ArgumentList "/i C:\cmake_installer.msi ADD_CMAKE_TO_PATH=All /qn"
 Remove-Item "C:\cmake_installer.msi"
 
