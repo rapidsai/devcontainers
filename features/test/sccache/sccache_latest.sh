8a9c19a3d4979244eb213e7651290b1b451d0f64
ptaylor <paul.e.taylor@me.com>
2025-04-01 12:55:59 -0700

    add test for latest

---
+++ features/test/sccache/sccache_latest.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# This test can be run with the following command (from the root of this repo)
+# ```
+# npx --package=@devcontainers/cli -c 'devcontainer features test \
+#     --features cuda \
+#     --base-image ubuntu:22.04 .'
+# ```
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "sccache exists and is on path" which sccache
+echo "sccache version: $(sccache --version)"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
