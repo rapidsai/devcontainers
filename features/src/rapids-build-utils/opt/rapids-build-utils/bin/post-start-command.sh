e7b1c1a141f1dfe5c9b4d5113fe46d676e489ff8
ptaylor <paul.e.taylor@me.com>
2025-04-08 13:25:26 -0700

    rapids-update-build-dir-links -j

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
@@ -2,7 +2,7 @@
 
 if test -z "${SKIP_RAPIDS_BUILD_UTILS_POST_START_COMMAND:-}"; then
     rapids-generate-scripts;
-    rapids-update-build-dir-links;
+    rapids-update-build-dir-links -j;
     rapids-make-vscode-workspace --update;
     rapids-merge-compile-commands-json > ~/compile_commands.json;
     if test -n "${PYTHON_PACKAGE_MANAGER:-}"; then
--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/update-build-dir-links.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/update-build-dir-links.sh
@@ -3,28 +3,20 @@
 # Usage:
 #  rapids-update-build-dir-links [OPTION]...
 #
-# List python package names as determined by manifest.yaml
+# Update the `build/latest` symlink for each library.
 #
 # Boolean options:
 #  -h,--help             Print this text.
+#  -v,--verbose          Verbose output.
 #
 # Options that require values:
+#  -j,--parallel <num>   Update <num> libraries in parallel
 # @_include_value_options rapids-list-repos -h | tail -n+2 | head -n-1;
 
 # shellcheck disable=SC1091
 . rapids-generate-docstring;
 
-update_build_dir_links() {
-    local -;
-    set -euo pipefail;
-
-    eval "$(_parse_args "$@" <&0)";
-
-    eval "$(rapids-list-repos "$@")";
-
-    # shellcheck disable=SC1091
-    . devcontainer-utils-debug-output 'rapids_build_utils_debug' 'update-build-dir-links';
-
+_get_library_paths() {
     local i;
     local j;
 
@@ -40,18 +32,36 @@ update_build_dir_links() {
             for ((j=0; j < ${!cpp_length:-0}; j+=1)); do
                 local cpp_sub_dir="${repo}_cpp_${j}_sub_dir";
                 local cpp_path=~/"${!repo_path:-}${!cpp_sub_dir:+/${!cpp_sub_dir}}";
-                rapids-get-cmake-build-dir --skip-build-type -- "${cpp_path}" >/dev/null;
+                echo "${cpp_path}";
             done
 
             for ((j=0; j < ${!py_length:-0}; j+=1)); do
                 local py_sub_dir="${repo}_python_${j}_sub_dir";
                 local py_path=~/"${!repo_path:-}${!py_sub_dir:+/${!py_sub_dir}}";
                 if rapids-python-uses-scikit-build-core "${py_path}"; then
-                    rapids-get-cmake-build-dir --skip-build-type -- "${py_path}" >/dev/null;
+                    echo "${py_path}";
                 fi
             done
         fi
     done
 }
 
-update_build_dir_links "$@" <&0;
+_update_build_dir_links() {
+    local -;
+    set -euo pipefail;
+
+    eval "$(_parse_args --take '-v,--verbose' "$@" <&0)";
+
+    eval "$(rapids-get-num-archs-jobs-and-load --archs 0 "$@")";
+
+    eval "$(rapids-list-repos "$@")";
+
+    # shellcheck disable=SC1091
+    . devcontainer-utils-debug-output 'rapids_build_utils_debug' 'update-build-dir-links';
+
+    _get_library_paths | tr '\n' '\0' \
+  | xargs ${v:+-t} -r -0 -n1 -P${n_jobs} bash -c \
+  " if ! rapids-get-cmake-build-dir --skip-build-type -- \"\$0\" >/dev/null; then exit 255; fi";
+}
+
+_update_build_dir_links "$@" <&0;
