1fff792fde75ca40e5e2948eec5e24b3fb97341e
Kyle Edwards <kyedwards@nvidia.com>
2025-04-28 17:20:14 -0400

    Debugging

--- features/src/gitlab-cli/install.sh
+++ features/src/gitlab-cli/install.sh
@@ -21,10 +21,8 @@ gitlab_cli_file_name() {
     local arch="${TARGETARCH:-}";
 
     if [[ "${CLI_VERSION}" < "1.47.0" ]]; then
-        echo "CLI_VERSION < 1.47.0" >&2;
         arch="${arch:-$(uname -m)}";
     else
-        echo "CLI_VERSION >= 1.47.0" >&2;
         arch="${arch:-$(dpkg --print-architecture | awk -F'-' '{print $NF}')}";
         os="${os,,}";
     fi
@@ -35,8 +33,10 @@ gitlab_cli_file_name() {
 download_gitlab_cli_deb() {
     local -;
     set -euo pipefail;
+    local filename="$(gitlab_cli_file_name)";
+    echo "GitLab CLI filename: $filename" >&2;
     wget --no-hsts -q -O /tmp/gitlab-cli.deb \
-        "https://gitlab.com/gitlab-org/cli/-/releases/v${CLI_VERSION}/downloads/$(gitlab_cli_file_name)";
+        "https://gitlab.com/gitlab-org/cli/-/releases/v${CLI_VERSION}/downloads/$filename";
 }
 
 # Fall back on direct download if no apt package exists
