10194cbebbaa35556a0f01d5e846936c07dbbc7a
Bradley Dice <bdice@bradleydice.com>
2025-08-07 02:02:47 -0500

    Add support for CUDA 13.0.0 (#556)
    This PR adds support for CUDA 13.0.0.
    
    CI jobs for RAPIDS builds still use CUDA 12.9 since not all libraries
    support 13.0 yet.
    
    Windows scripts and some tests are left using CUDA 12.9.
    
    ---------
    
    Co-authored-by: Allison Piper <alliepiper16@gmail.com>
    Co-authored-by: Paul Taylor <178183+trxcllnt@users.noreply.github.com>

--- features/src/cuda/install.sh
+++ features/src/cuda/install.sh
@@ -30,7 +30,7 @@ export OSNAME="$(
     echo "$ID$((major - (major % 2)))${minor}";
 )";
 
-VERSION="${CUDA_VERSION:-${VERSION:-12.9.0}}";
+VERSION="${CUDA_VERSION:-${VERSION:-13.0.0}}";
 
 if [[ "$NVARCH" == aarch64 ]]; then
     NVARCH="sbsa";
