b89015e704b16330c013dfd346b30fb98fe9a185
Wesley Maxey <wesley.maxey@gmail.com>
2025-10-01 14:37:19 -0700

    Fixup missing entry in buildenv

--- windows/image/installers/build-env.ps1
+++ windows/image/installers/build-env.ps1
@@ -42,6 +42,7 @@ $MSBuildPathMap = @{
     "14.41"="$MSBuildPath\17\VC\Auxiliary\Build"
     "14.42"="$MSBuildPath\17\VC\Auxiliary\Build"
     "14.43"="$MSBuildPath\17\VC\Auxiliary\Build"
+    "14.44"="$MSBuildPath\17\VC\Auxiliary\Build"
     "14.50"="$MSBuildPath\18\VC\Auxiliary\Build"
     "latest"="$MSBuildPath\$ENV:INSTALLED_MSVC_VERSION\VC\Auxiliary\Build"
 }
