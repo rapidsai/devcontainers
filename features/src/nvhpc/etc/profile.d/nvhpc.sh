b8fafc7b86b3e7ff417bcc9004cb25c6964dd67f
ptaylor <paul.e.taylor@me.com>
2025-12-04 12:46:11 -0800

    fix find

--- features/src/nvhpc/etc/profile.d/nvhpc.sh
+++ features/src/nvhpc/etc/profile.d/nvhpc.sh
@@ -22,7 +22,7 @@ if [ -n "${PATH##*"${NVHPC_ROOT}/compilers/bin"*}" ]; then
     if [ "${NVHPC_VERSION_MAJOR}" -ge 25 ] \
     && [ "${NVHPC_VERSION_MINOR}" -ge 7 ]; then
         set -x;
-        HPCX_INIT="$(find -L "$NVHPC_ROOT"/comm_libs/ -path '*/latest/hpcx-init.sh' -quit)";
+        HPCX_INIT="$(find -L "$NVHPC_ROOT"/comm_libs/ -path '*/latest/hpcx-init.sh' -print -quit)";
         if [ -n "${HPCX_INIT:+x}" ] && [ -s "${HPCX_INIT}" ]; then
             . "$HPCX_INIT";
             hpcx_load;
