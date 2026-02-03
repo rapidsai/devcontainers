425542002ad57f1e4923764c7b49b7f6f6f43990
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2026-02-03 15:34:42 -0800

    Fix local dev scripts (#655)
    Fixes local dev scripts (`./scripts/build.sh`, `./scripts/run.sh`) after
    recent changes to publish images with latest tags.

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,10 +12,12 @@ run_devcontainer() {
     # Generate the devcontainer
     eval "$(./scripts/generate.sh "$@" | xargs -r -d'\n' -I% echo -n local %\;)";
 
-    echo "tag=${tag-}" >&2;
-    echo "workspace=${workspace-}" >&2;
+    echo "tag=${tag:-}" >&2;
+    echo "workspace=${workspace:-}" >&2;
 
-    if ! test -n "${workspace-}" || ! test -n "${tag-}"; then exit 1; fi;
+    if ! test -n "${workspace:-}" || ! test -n "${tag:-}"; then exit 1; fi;
+
+    tag="${version:-latest}-${tag}";
 
     # Print the generated devcontainer JSON
     cat "${workspace}/.devcontainer/devcontainer.json" >&2;
@@ -62,42 +64,51 @@ run_devcontainer() {
     vars+=(--remote-env "SCCACHE_REGION=${sccache_region}");
 
     # Start the devcontainer
-    local -r container_id="$(                                                                                                       \
-        devcontainer up                                                                                                             \
-            "${vars[@]}"                                                                                                            \
-            --skip-post-attach                                                                                                      \
-            --remove-existing-container                                                                                             \
-            --terminal-rows "${lines}"                                                                                              \
-            --terminal-columns "${columns}"                                                                                         \
-            --mount-workspace-git-root false                                                                                        \
-            --workspace-folder "${workspace}"                                                                                       \
-            --cache-from "docker.io/rapidsai/devcontainers:${tag}"                                                                  \
-            --mount "type=bind,source=${SSH_AUTH_SOCK},target=/tmp/ssh-auth-sock"                                                   \
-            --mount "type=bind,source=$(pwd)/.scratch/.aws,target=/home/coder/.aws"                                                 \
-            --mount "type=bind,source=$(pwd)/.scratch/.cache,target=/home/coder/.cache"                                             \
-            --mount "type=bind,source=$(pwd)/.scratch/.config,target=/home/coder/.config"                                           \
-            --mount "type=bind,source=$(pwd)/features/src/utils/opt/devcontainer,target=/opt/devcontainer"                          \
-            --additional-features '{ "./features/src/rapids-build-utils": {} }'                                                     \
-            --mount "type=bind,source=$(pwd)/features/src/rapids-build-utils/opt/rapids-build-utils,target=/opt/rapids-build-utils" \
-      | jq -r '.containerId'
-    )";
-
-    local -r image_tag="$(
-        docker image inspect --format '{{index (split (index .RepoTags 0) ":") 0}}' "$(
-            docker inspect "${container_id}" --format '{{index (split .Image ":") 1}}'
-        )"
-    )";
+    devcontainer up                                                                                                             \
+        "${vars[@]}"                                                                                                            \
+        --skip-post-attach                                                                                                      \
+        --remove-existing-container                                                                                             \
+        --terminal-rows "${lines}"                                                                                              \
+        --terminal-columns "${columns}"                                                                                         \
+        --mount-workspace-git-root false                                                                                        \
+        --workspace-folder "${workspace}"                                                                                       \
+        --cache-from "docker.io/rapidsai/devcontainers:${tag}"                                                                  \
+        --mount "type=bind,source=${SSH_AUTH_SOCK},target=/tmp/ssh-auth-sock"                                                   \
+        --mount "type=bind,source=$(pwd)/.scratch/.aws,target=/home/coder/.aws"                                                 \
+        --mount "type=bind,source=$(pwd)/.scratch/.cache,target=/home/coder/.cache"                                             \
+        --mount "type=bind,source=$(pwd)/.scratch/.config,target=/home/coder/.config"                                           \
+        --mount "type=bind,source=$(pwd)/features/src/utils/opt/devcontainer,target=/opt/devcontainer"                          \
+        --additional-features '{ "./features/src/rapids-build-utils": {} }'                                                     \
+        --mount "type=bind,source=$(pwd)/features/src/rapids-build-utils/opt/rapids-build-utils,target=/opt/rapids-build-utils" \
+  | tee "${workspace}/devcontainer-up.log";
 
     # shellcheck disable=SC2317
     cleanup_2() {
         code=$?;
         set -x;
         find ./features/src -maxdepth 1 -type d -name '*\.[0-9]' -exec rm -r "{}" \;
-        echo "Removing ${container_id}";
-        docker rm -f "${container_id}" >/dev/null 2>&1 || true;
-        docker rmi -f "${image_tag}" >/dev/null 2>&1 || true;
-        docker rmi -f "${image_tag%%-uid*}" >/dev/null 2>&1 || true;
+
+        local -r container_id="$(jq -r '.containerId' "${1}/devcontainer-up.log")";
+
+        if test -n "${container_id:+x}"; then
+            echo "Removing ${container_id}";
+
+            local -r image_tag="$(
+                docker image inspect --format '{{index (split (index .RepoTags 0) ":") 0}}' "$(
+                    docker inspect "${container_id}" --format '{{index (split .Image ":") 1}}'
+                )"
+            )";
+
+            docker rm -f "${container_id}" >/dev/null 2>&1 || true;
+
+            if test -n "${image_tag:+x}"; then
+                docker rmi -f "${image_tag}" >/dev/null 2>&1 || true;
+                docker rmi -f "${image_tag%%-uid*}" >/dev/null 2>&1 || true;
+            fi
+        fi
+
         rm -rf "${1}";
+
         exit "${code}";
     }
 
