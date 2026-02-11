3d5c88c2dc197c296c80a00e5c7250aa90226a9a
ptaylor <paul.e.taylor@me.com>
2026-02-10 17:49:35 -0800

    install the full cudss-cuda-<ver> package

--- features/src/cuda/install.sh
+++ features/src/cuda/install.sh
@@ -187,10 +187,7 @@ if [ "${INSTALLCUDNN:-false}" = true ]; then
 fi
 
 if "${INSTALLCUDSS:-false}"; then
-    PKGS+=("libcudss0${dev_tag}-cuda-${cuda_ver_major}");
-    if ! "${PRUNESTATICLIBS:-false}"; then
-        PKGS+=("libcudss0-static-cuda-${cuda_ver_major}");
-    fi
+    PKGS+=("cudss-cuda-${cuda_ver_major}");
 fi
 
 if [ "${INSTALLNCCL:-false}" = true ] \
