7f0130fc8bfe6edd0ac1ff2d0ac5219a1719ca8e
ptaylor <paul.e.taylor@me.com>
2026-03-06 08:41:50 -0800

    exit with error if creating python environment fails

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
@@ -6,7 +6,7 @@ if ! test -n "${SKIP_RAPIDS_BUILD_UTILS_POST_START_COMMAND:+x}"; then
     rapids-make-vscode-workspace --update &
     rapids-merge-compile-commands-json > ~/compile_commands.json &
     if test -n "${PYTHON_PACKAGE_MANAGER:+x}"; then
-        rapids-make-"${PYTHON_PACKAGE_MANAGER}"-env "$@" || true;
+        rapids-make-"${PYTHON_PACKAGE_MANAGER}"-env "$@";
     fi
     wait
 fi
