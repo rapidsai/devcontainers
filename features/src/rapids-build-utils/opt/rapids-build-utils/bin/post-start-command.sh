fd8dadbfdeab4a97107f3819ad735dee656c3f8d
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2026-03-10 09:34:36 -0700

    exit with error if creating python environment fails (#670)

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/post-start-command.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-if ! test -n "${SKIP_RAPIDS_BUILD_UTILS_POST_START_COMMAND:+x}"; then
+_rapids_post_start_command() {
+    local -;
+    set -euo pipefail;
+
     rapids-generate-scripts;
     rapids-update-build-dir-links -j &
     rapids-make-vscode-workspace --update &
     rapids-merge-compile-commands-json > ~/compile_commands.json &
     if test -n "${PYTHON_PACKAGE_MANAGER:+x}"; then
-        rapids-make-"${PYTHON_PACKAGE_MANAGER}"-env "$@" || true;
+        rapids-make-"${PYTHON_PACKAGE_MANAGER}"-env "$@";
     fi
     wait
+}
+
+if ! test -n "${SKIP_RAPIDS_BUILD_UTILS_POST_START_COMMAND:+x}"; then
+    _rapids_post_start_command "$@" <&0;
 fi
