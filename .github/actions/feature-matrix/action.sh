343edf5681e12e3e9f7962cb81280f6977182e11
ptaylor <paul.e.taylor@me.com>
2026-06-25 15:15:11 -0700

    parallelize feature scenario tests

--- .github/actions/feature-matrix/action.sh
+++ .github/actions/feature-matrix/action.sh
@@ -8,9 +8,9 @@ join_with_delimiter() {
 }
 
 find_features_with_tests() {
-  find ${@:2} -mindepth 1 -type f -name "$1" ! -wholename '*/test/_global/*' \
-    -exec bash -c 'echo {} | sed -r s@features/test/\(.*\)/.*@\\1@' \;       \
-  | sort | uniq                                                              ;
+  find "${@:2}" -mindepth 1 -type f -name "$1" ! -wholename '*/test/_global/*' \
+    -exec bash -c 'echo {} | sed -r s@features/test/\(.*\)/.*@\\1@' \;         \
+  | sort | uniq                                                                ;
 }
 
 full_matrix="0";
@@ -22,35 +22,40 @@ esac
 
 files="${@}";
 
-features="";
-scenarios="";
+declare -a features=();
+declare -a scenarios=();
 
 if [ "${full_matrix}" == "1" ] || grep -q "\.github/" <<< "$files"; then
-  features="$(find_features_with_tests 'test.sh' 'features/test')";
-  scenarios="$(find_features_with_tests 'scenarios.json' 'features/test')";
+  declare -a features="($(find_features_with_tests 'test.sh' 'features/test'))"
+  declare -a scenarios="($(find_features_with_tests 'scenarios.json' 'features/test'))"
 elif echo "$files" | grep -q "features/"; then
-  files="$(join_with_delimiter "\n" $files | grep "features/")";
-  files="$(echo -e "$files")";
+  files="$(join_with_delimiter "\n" $files | grep "features/")"
+  declare -a files="($(echo -e "$files"))"
   changed=();
-  for x in ${files}; do
+  for x in "${files[@]}"; do
     x="${x##features/src/}";
     x="${x##features/test/}";
     x="features/test/${x%%/*}";
     changed+=("${x}");
-  done;
-  changed="${changed[@]}";
-  features="$(find_features_with_tests 'test.sh' ${changed})";
-  scenarios="$(find_features_with_tests 'scenarios.json' ${changed})";
+  done
+  declare -a features="($(find_features_with_tests 'test.sh' "${changed[@]}"))"
+  declare -a scenarios="($(find_features_with_tests 'scenarios.json' "${changed[@]}"))"
 fi
 
-if [[ -n "$(echo "$features")" ]]; then
-  features="[\"$(join_with_delimiter '","' $features)\"]";
+if test "${#features[@]}" -gt 0; then
+  features="[\"$(join_with_delimiter '","' "${features[@]}")\"]";
 fi
 
 echo "features=${features:-[]}";
 
-if [[ -n "$(echo "$scenarios")" ]]; then
-  scenarios="[\"$(join_with_delimiter '","' $scenarios)\"]";
+if test "${#scenarios[@]}" -gt 0; then
+  declare -a scenarios_json=()
+  for feature in "${scenarios[@]}"; do
+    if test -f "features/test/$feature/scenarios.json"; then
+        scenarios_json+=("{\"feature\": \"$feature\", \"scenarios\": $(jq -cM "keys | @json" "features/test/$feature/scenarios.json" | tr -d '\n')}")
+    fi
+  done
+  scenarios="[$(IFS=,; echo "${scenarios_json[*]}")]";
 fi
 
 echo "scenarios=${scenarios:-[]}";
