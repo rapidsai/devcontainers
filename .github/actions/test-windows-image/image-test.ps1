1a93ca868a6b225993399a16b6f1b5bbb9b237c6
David Bayer <bayer.david99@gmail.com>
2026-09-17 07:39:30 +0200

    try to set C++ standard version

--- .github/actions/test-windows-image/image-test.ps1
+++ .github/actions/test-windows-image/image-test.ps1
@@ -5,13 +5,11 @@ function TestReturnCode {
 }
 
 $mismatch_nvcc_cl_flags = @(
+    '-std=c++17'
     # Tell NVCC that old msvc is okay
     '--allow-unsupported-compiler',
     # Tell MSVC that new nvcc is okay
     '-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH'
-    # Since MSVC 19.51, there is a static assertion triggered when compiling with nvcc < 13.0 regarding
-    # std::aligned_storage conformance. By defining _ENABLE_EXTENDED_ALIGNED_STORAGE, we select the conforming behaviour
-    '-D_ENABLE_EXTENDED_ALIGNED_STORAGE'
 )
 
 $ErrorActionPreference = "Stop"
